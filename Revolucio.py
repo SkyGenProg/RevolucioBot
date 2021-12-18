2bab50d84e823de601fc6988c7f02b20d5524826
Simon Kempf <simon25042001@gmail.com>
2021-12-18 16:43:19 +0100

    create dir

--- Revolucio.py
+++ Revolucio.py
@@ -7,10 +7,11 @@ from includes.wiki_tasks import *
 from includes.vikidia import *
 from config import *
 
-os.chdir("files")
-
 if __name__ == "__main__":
     print("Revolució %s %s." % (ver, lang))
+    if(!os.path.exists("files"):
+       os.mkdir("files")
+    os.chdir("files")
     #dicoado_site = get_wiki(user_wiki=user_bot, lang=lang, family="dicoado")
     vikidia_task = wiki_task(wikis[0], user_bot)
     #dicoado_task = wiki_task(site=dicoado_site, user_bot=user_bot)
