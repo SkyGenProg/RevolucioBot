db33eab43c3f8a9bdc905b92a18dd9fa7453588e
Simon Kempf <simon25042001@gmail.com>
2021-12-19 21:19:35 +0100

    Webhook

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ver = "5.0.0"
 release_date = "19/12/2021"
 
 webhooks_url = {'vikidia': "REMOVED",
-                'dicoado': None} #Ajouter les nouveaux wikis
+                'dicoado': "REMOVED"} #Ajouter les nouveaux wikis
 headers = {
     'content-type': 'application/json',
     'user-agent': 'Mozilla/5.0 Revolucio'
