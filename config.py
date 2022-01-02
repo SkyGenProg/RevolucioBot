c3cff8d7c7da78df39b6fd67bc391f95d6cb9227
Simon Kempf <simon25042001@gmail.com>
2022-01-02 21:21:52 +0100

    bug

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 import argparse, getpass
 
 ver = "5.1.0-beta.1"
-release_date = "03/01/2022"
+release_date = "02/01/2022"
 
 webhooks_url = {'localhost': "https://discord.com/api/webhooks/922633624912814120/Rx1F2e81k9IbBYd32f9txntk_yMVEirVlTVfqMKGdKGcLPM_sZfoFoPxa1C2JbBbFlQz",
                 'vikidia': "REMOVED",
