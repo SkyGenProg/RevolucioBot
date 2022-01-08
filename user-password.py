73c5b0df2b0e1349f181c793663c955d86b2f436
Simon Kempf <simon25042001@gmail.com>
2022-01-08 17:46:21 +0100

    rename

--- user-password.py
+++ user-password.py
@@ -1,4 +1,4 @@
 ('localhost', 'localhost', 'RevolucioBot', BotPassword('Revolucio', 'h99poj37bh8c2vsqj13tn65oma9f0f6v'))
 ('fr', 'vikidia', 'RevolucioBot', BotPassword('Revolucio', 'REMOVED'))
-('en', 'vikidia', 'Revolucio', BotPassword('Revolucio', 'q7ubpk9jqvo94bstj1qcfh0oldpbsoti'))
+('en', 'vikidia', 'RevolucioBot', BotPassword('Revolucio', 'q7ubpk9jqvo94bstj1qcfh0oldpbsoti'))
 ('dicoado', 'dicoado', 'RevolucioBot', BotPassword('Revolucio', 'REMOVED'))
