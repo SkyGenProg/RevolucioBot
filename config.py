fa27b540a9f2b49ff0d0a51e91a6eaabed198188
Simon Kempf <simon25042001@gmail.com>
2022-06-08 12:37:03 +0200

    5.4.0

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-ver = "5.4.0-beta.1"
-release_date = "29/04/2022"
+ver = "5.4.0"
+release_date = "08/06/2022"
 
 webhooks_url = {'localhost': "https://discord.com/api/webhooks/922633624912814120/Rx1F2e81k9IbBYd32f9txntk_yMVEirVlTVfqMKGdKGcLPM_sZfoFoPxa1C2JbBbFlQz",
                 'vikidia': "REMOVED",
